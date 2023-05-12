419b24e3ef324fae23659eedaba26ed58e61573f
ThomasAston <tjn.aston@gmail.com>
2023-05-12 09:42:47 +0100

    updating conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -33,8 +33,8 @@ import mock
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-# sys.path.insert(0, os.path.abspath(os.path.join("EdiHeadyTrack")))
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath(os.path.join("EdiHeadyTrack")))
+# sys.path.insert(0, os.path.abspath('..'))
 
 print('Attempting EdiHeadyTrack import...')
 import EdiHeadyTrack
