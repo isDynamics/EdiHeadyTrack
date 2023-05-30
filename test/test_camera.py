4db70c1811f191c14385e8ae0a8d18180b843607
ThomasAston <tjn.aston@gmail.com>
2023-05-30 11:14:56 +0100

    updating test_camera

--- test/test_camera.py
+++ test/test_camera.py
@@ -3,7 +3,7 @@ from EdiHeadyTrack.camera import Camera, Checkerboard, Calibrator
 TEST_FILE = 'resources/calibration_example.mp4'
 from EdiHeadyTrack.video import Video
 TEST_VIDEO = Video(TEST_FILE)
-SHOW = True
+SHOW = False
 CHECKERBOARD = (9,6)
 
 def test_Camera_calibrate():
@@ -16,37 +16,3 @@ def test_Camera_calibrate():
     camera.calibrator.save_outputs()
     assert os.path.exists('camera_matrix.csv')
     assert os.path.exists('camera_distortion.csv')
-
-# def test_initialise():
-#     '''
-#     Assert correct values are returned for dummy checkerboard dimensions
-#     '''
-#     checkerboard = (2,2)
-#     criteria, threedpoints, twodpoints, objectp3d = EdiHeadyTrack.calibrating.initialise(checkerboard)
-#     assert criteria == (3, 30, 0.001)
-#     assert threedpoints == []
-#     assert twodpoints == []
-#     import numpy as np
-#     assert np.array_equal(objectp3d, [[[0., 0., 0.],[1., 0., 0.],[0., 1., 0.],[1., 1., 0.]]])
-
-
-# def test_vid():
-#     '''
-#     Assert frames of video file can be read, and that video fps is greater than 0
-#     '''
-#     assert EdiHeadyTrack.calibrating.open_vid(TEST_FILE)[0].read()[0] == True
-#     assert EdiHeadyTrack.calibrating.open_vid(TEST_FILE)[1] > 0
-
-
-# def test_calibrate():
-#     '''
-#     Assert that calibrate function runs without error
-#     '''
-#     assert EdiHeadyTrack.calibrating.calibrate(TEST_FILE) == None
-
-# def test_webcam():
-#     '''
-#     Assert frames from webcam can be read, and that video fps is greater than 0
-#     '''
-#     assert EdiHeadyTrack.calibrating.open_vid(0)[0].read()[0] == True
-#     assert EdiHeadyTrack.calibrating.open_vid(0)[1] > 0
---
+++ test/test_facedetector.py
@@ -0,0 +1,8 @@
+from EdiHeadyTrack.facedetector import FaceDetector, MediaPipe
+
+TEST_FILE = 'resources/testvid.mp4'
+from EdiHeadyTrack import Video
+TEST_VIDEO = Video(TEST_FILE)
+
+def test_FaceDetector():
+    ...
